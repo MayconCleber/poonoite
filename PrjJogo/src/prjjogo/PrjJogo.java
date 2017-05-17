036e51ffde729246bf8248c95633424e358c4e20
maromo71 <professormoraes@gmail.com>
2017-05-16 22:22:28 -0300

    Jogo da Veia

---
+++ PrjJogo/src/prjjogo/PrjJogo.java
@@ -0,0 +1,22 @@
+/*
+ * Aulas professor Maromo.
+ */
+package prjjogo;
+
+/**
+ *
+ * @author maromo
+ */
+public class PrjJogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmJogoVeia tela = new FrmJogoVeia();
+        tela.setTitle("Jogo da Véia");
+        tela.setVisible(true);
+        
+    }
+    
+}
